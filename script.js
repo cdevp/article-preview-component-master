e440b3bed2b81fb5e4340711494931beebf104ad
cdevp <>
2020-11-09 13:45:51 -0500

    Finished landscape view

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById('arrow-background').addEventListener('click', () => {
+    if(document.getElementById('social-links').style.display === 'none' ||
+        document.getElementById('social-links').style.display === '') {
+        document.getElementById('social-links').style.display = 'flex';
+        document.getElementById('arrow-background').style.backgroundColor = 'hsl(214, 17%, 51%)';
+        document.getElementById('arrow').style.display = 'none';
+        document.getElementById('white-arrow').style.display = 'flex';
+    }
+    else {
+        document.getElementById('social-links').style.display = 'none';
+        document.getElementById('arrow-background').style.backgroundColor = 'hsl(210, 46%, 95%)';
+        document.getElementById('arrow').style.display = 'flex';
+        document.getElementById('white-arrow').style.display = 'none';
+   }
+
+});
